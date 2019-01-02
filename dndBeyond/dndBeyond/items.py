e92fcd5972880b3902b0e57a681111fdb881d63d
further-reading <roy.healy87+gh@gmail.com>
2019-01-02 20:57:54 +0000

    Added check for skills

--- dndBeyond/dndBeyond/items.py
+++ dndBeyond/dndBeyond/items.py
@@ -21,7 +21,22 @@ def mapStats(statList):
 
     yield stats
 
+def mapSkills(skillList):
+    skillsChunks = [skillList[x:x + 4] for x in range(0, len(skillList), 4)]
+    skills = {}
+    for chunk in skillsChunks:
+        attribute = chunk[0]
+        skill = chunk[1]
+        modValue = int(chunk[2] + chunk[3])
+        skills[skill] = {'attribute': attribute,
+                         'modifier': modValue}
+
+    yield skills
+
+
 class DndbeyondItem(scrapy.Item):
     name = scrapy.Field(output_processor = TakeFirst())
     stats = scrapy.Field(input_processor = mapStats,
+                        output_processor = TakeFirst())
+    skills = scrapy.Field(input_processor = mapSkills,
                         output_processor = TakeFirst())
--- dndBeyond/dndBeyond/spiders/character_spider.py
+++ dndBeyond/dndBeyond/spiders/character_spider.py
@@ -11,6 +11,10 @@ class CharacterSpider(scrapy.Spider):
 
     def parse(self, response):
         loader = ItemLoader(item=DndbeyondItem(), response=response)
-        loader.add_xpath('name', "//div[@class='ct-character-tidbits__name']//text()")
-        loader.add_xpath('stats',"//div[@class='ct-quick-info__abilities']//text()")
+        loader.add_xpath('name',
+                         "//div[@class='ct-character-tidbits__name']//text()")
+        loader.add_xpath('stats',
+                         "//div[@class='ct-quick-info__abilities']//text()")
+        loader.add_xpath('skills',
+                         "//div[@class='ct-skills__item']//text()")
         yield loader.load_item()
